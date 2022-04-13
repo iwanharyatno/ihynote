3114aafcd7f6551bb6ee283ad8ece122edf91832
iwanharyatno <iwnharry61@gmail.com>
2022-04-13 15:23:37 +0700

    Mengarahkan pengguna ke halaman beranda setelah logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -38,6 +38,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/notes');
+        return redirect('/');
     }
 }
