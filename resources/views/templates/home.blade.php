01ee0ea0094a691086349076e87df6b2209233ee
iwanharyatno <iwnharry61@gmail.com>
2022-04-24 07:47:30 +0700

    Mengubah warna tema untuk browser

--- resources/views/templates/home.blade.php
+++ resources/views/templates/home.blade.php
@@ -22,7 +22,7 @@
 	<link rel="manifest" href="/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
+	<meta name="theme-color" content="#1d1d1d">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
